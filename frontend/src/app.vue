e12482fd3045e1f1cb129313c8a249cbb3e1d0d7
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-26 16:54:14 +0300

    Deleted write session

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -22,7 +22,6 @@ export default {
         router-link(to="/")
           img(src="@/assets/mylogo.png" style="width: auto; height: auto; max-width: 100px; max-height: 100px;")
     #nav
-      router-link(to="/write") Write
       router-link(to="/books") Books
       router-link(to="/profile") Profile
       router-link(to="/login") Login
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,6 @@ import Register from '../views/register.vue'
 import Profile from "../views/profile.vue"
 import Home from "../views/home.vue"
 import Books from "../views/books.vue"
-import Write from "../views/write.vue"
 Vue.use(VueRouter)
 
 export default function init(store) {
@@ -17,10 +16,6 @@ export default function init(store) {
         path: '/',
         name: 'Home',
         component: Home,
-      },
-      {
-        path: '/Write',
-        component: Write,
       },
       {
         path: '/books',
